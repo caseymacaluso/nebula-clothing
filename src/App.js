3caf518e5dff073e91b5326e3596368d72560374
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-08 11:20:08 -0500

    create component structure for categories

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Categories from "./components/categories/categories.component";
+
 import "./categories.styles.scss";
 
 const App = () => {
@@ -29,26 +31,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => {
-        return (
-          <div className="category-container" key={id}>
-            <div
-              className="background-image"
-              style={{
-                backgroundImage: `url(${imageUrl})`,
-              }}
-            />
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <Categories categories={categories} />;
 };
 
 export default App;
