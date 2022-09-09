e80b0d61f42a4ec5807b7907b63501424a0e7190
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-09 18:03:31 -0500

    add comments for better clarity

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+// Default values for form fields, will use for state management
 const defaultFormFields = {
   displayName: "",
   email: "",
@@ -13,31 +14,40 @@ const defaultFormFields = {
 
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
-  const { displayName, email, password, confirmPassword } = formFields;
+  const { displayName, email, password, confirmPassword } = formFields; // Destructure from formFields object
 
   const onInputChange = evt => {
+    // Destructure from evt.target
     const { name, value } = evt.target;
+    // spread other objects of state, and only update the state of the field with the matching name (i.e. email, password, etc.)
     setFormFields({ ...formFields, [name]: value });
   };
 
+  // Function to reset the form fields after the form is submitted
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
 
   const handleSubmit = async evt => {
+    // Prevents a page refresh on submit
     evt.preventDefault();
+    // Does not proceed if password fields don't match
     if (password !== confirmPassword) {
       alert("Passwords do not match. Try again");
       return;
     }
 
     try {
+      // Destructures user from the createAuthUserWithEmailAndPassword function
       const { user } = await createAuthUserWithEmailAndPassword(
         email,
         password
       );
 
+      // Creates a user document, but passes in the displayName as an additional argument
+      // without, the displayName ends up being null, and so the document will not be created
       await createUserDocumentFromAuth(user, { displayName });
+      // Resets form fields after document is created
       resetFormFields();
     } catch (e) {
       if (e.code === "auth/email-already-in-use") {
