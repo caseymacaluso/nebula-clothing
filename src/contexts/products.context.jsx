0a45fd51821b49ea61dfb808305b11d87db7ab2a
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-14 17:59:19 -0500

    add util functions for adding collection and getting categories

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,28 @@
-import { createContext, useEffect, useState } from "react";
-import PRODUCT_DATA from "../shop-data.json";
+import { createContext, useState, useEffect } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+// import PRODUCT_DATA from "../shop-data.json";
+// import SHOP_DATA from "../shop-data";
 
 export const ProductsContext = createContext({
   productData: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [productData, setProductData] = useState(PRODUCT_DATA);
+  const [productData, setProductData] = useState([]);
+
+  // Done as a one-off to populate the database on Firebase
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+
+  useEffect(() => {
+    const getCategoryMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoryMap();
+  }, []);
+
   const value = { productData };
 
   return (
