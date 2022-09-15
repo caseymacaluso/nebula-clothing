a8f0ae91887a42f3a9713b6132a802ac944e7bec
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-15 16:19:14 -0500

    renamed category and category-item to directory and directory-item, respectively

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,6 +1,6 @@
-import "./category-item.styles.scss";
+import "./directory-item.styles.scss";
 
-const CategoryItem = ({ category }) => {
+const DirectoryItem = ({ category }) => {
   const { title, imageUrl } = category;
   return (
     <div className="category-container">
@@ -18,4 +18,4 @@ const CategoryItem = ({ category }) => {
   );
 };
 
-export default CategoryItem;
+export default DirectoryItem;
