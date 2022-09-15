4113124d688585109df3e76640d5bfa3b5149452
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-15 17:49:17 -0500

    add link to category preview, modified some styling for directory-item

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "./category-preview.styles.scss";
 import ProductCard from "../product-card/product-card-component";
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
-      <span className="title">{title.toUpperCase()}</span>
+      <Link className="title" to={title}>
+        {title.toUpperCase()}
+      </Link>
       <div className="preview">
         {products
           .filter((_, idx) => idx < 4)
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,16 +1,17 @@
+// import { Link } from "react-router-dom";
 import "./directory-item.styles.scss";
 
 const DirectoryItem = ({ category }) => {
   const { title, imageUrl } = category;
   return (
-    <div className="category-container">
+    <div className="directory-item-container">
       <div
         className="background-image"
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="category-body-container">
+      <div className="body">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
