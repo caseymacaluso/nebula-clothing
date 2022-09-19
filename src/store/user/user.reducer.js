2945ec2c6f1bf3b4ce8078a655656ff60e5c5201
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-19 18:54:42 -0500

    added redux libraries and beginning setup for redux stores

---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,17 @@
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+export const userReducer = (state = INITIAL_STATE, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      return state; // with reducers, every reducer receives every action, so if a specific reducer does not receive a specific action, the current state should be returned.
+  }
+};
