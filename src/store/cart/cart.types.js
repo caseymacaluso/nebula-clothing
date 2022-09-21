957fb92d5cd16120075e45eeaac40f32fce4c89c
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-21 17:00:18 -0500

    added reselect package, added  cartReducer logic

---
+++ src/store/cart/cart.types.js
@@ -0,0 +1,7 @@
+// Action types for cart functions.
+export const CART_ACTION_TYPES = {
+  SET_CART_ITEMS: "cart/SET_CART_ITEMS",
+  SET_IS_CART_OPEN: "cart/SET_IS_CART_OPEN",
+  SET_CART_COUNT: "cart/SET_CART_COUNT", // currently not in use, derived from selectors
+  SET_CART_TOTAL: "cart/SET_CART_TOTAL", // currently not in use, derived from selectors
+};
