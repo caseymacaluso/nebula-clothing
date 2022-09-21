c8d0ecad435abb922fe8fbec4e165cba34a504db
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-21 18:23:56 -0500

    added redux-persist library, added config to persist state through browser refresh

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { compose, createStore, applyMiddleware } from "redux";
 // import logger from "redux-logger";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // root-reducer
 import { rootReducer } from "./root-reducer";
 
@@ -8,15 +9,31 @@ const loggerMiddleware = store => next => action => {
   if (!action.type) {
     return next(action);
   }
+  console.log("-------------------------------------");
   console.log("type", action.type);
   console.log("payload", action.payload);
   console.log("current state", store.getState());
   next(action);
   console.log("next state", store.getState());
+  console.log("-------------------------------------");
 };
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
