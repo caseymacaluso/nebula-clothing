8107619a037eaf44f2bdac6f42623de4ac29194d
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-23 13:54:13 -0500

    converted categories redux from thunk to saga

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "@redux-saga/core";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 // root-reducer
 import { rootReducer } from "./root-reducer";
+import { rootSaga } from "./root-saga";
 
 // Configuration for Redux Persist
 const persistConfig = {
@@ -15,11 +17,13 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const sagaMiddleWare = createSagaMiddleware();
+
 // Don't want to pass false to middleware, so we filter and pass an empty array when the env is production
 // Thunk is included in the middlewares variable to support thunk operations
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleWare,
 ].filter(Boolean);
 
 // Composer that enables Redux Dev Tools Chrome extension for development purposes. If redux devtools extension doesn't exist, use the normal compose from redux.
@@ -36,5 +40,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleWare.run(rootSaga);
 
 export const persistor = persistStore(store);
