6983c6ab786bfb5d90e765d7fe6f44d77a94fb6a
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-23 17:58:55 -0500

    add more cases for user reducer

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -4,6 +4,8 @@ import { USER_ACTION_TYPES } from "./user.types";
 // initial state for the user
 const INITIAL_STATE = {
   currentUser: null,
+  isLoading: false,
+  error: null,
 };
 
 // Reducer that takes the state and an action as arguments, both of which are initialized with default values.
@@ -13,8 +15,14 @@ export const userReducer = (state = INITIAL_STATE, action) => {
   // Goes through different user action types to see what to do
   switch (type) {
     // if type is SET_CURRENT_USER, spread over current state and set currentUser equal to the payload
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
+    case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
       return { ...state, currentUser: payload };
+    case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
+      return { ...state, currentUser: null };
+    case USER_ACTION_TYPES.SIGN_IN_FAILED:
+    case USER_ACTION_TYPES.SIGN_UP_FAILED:
+    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
+      return { ...state, error: payload };
     default:
       return state; // with reducers, every reducer receives every action, so if a specific reducer does not receive a specific action, the current state should be returned.
   }
