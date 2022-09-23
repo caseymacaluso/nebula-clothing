3b040efb9dfe467d74bd8981c09368897041a748
Casey Macaluso <macaluso.casey@gmail.com>
2022-09-23 17:57:56 -0500

    replace unsubscribe with checkUserSession saga action

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
+  getCurrentUser,
 } from "./utils/firebase/firebase.utils";
-import { setCurrentUser } from "./store/user/user.action";
+import { checkUserSession, setCurrentUser } from "./store/user/user.action";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
@@ -15,14 +16,18 @@ import Checkout from "./routes/checkout/checkout.component";
 const App = () => {
   const dispatch = useDispatch();
 
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChangedListener(user => {
+  //     if (user) {
+  //       createUserDocumentFromAuth(user);
+  //     }
+  //     dispatch(setCurrentUser(user));
+  //   });
+  //   return unsubscribe;
+  // });
+
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener(user => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-    return unsubscribe;
+    dispatch(checkUserSession());
   });
 
   return (
