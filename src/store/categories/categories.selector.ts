6b699fbe3d2103dfe71a337706083643bd6b8ddd
Casey Macaluso <macaluso.casey@gmail.com>
2022-10-04 19:39:40 -0500

    beginning typescript integration with categories info

--- src/store/categories/categories.selector.ts
+++ src/store/categories/categories.selector.ts
@@ -3,8 +3,11 @@
 /////////////////////////////////////////
 import { createSelector } from "reselect";
 
+import { CategoriesState } from "./categories.reducer";
+import { CategoryMap } from "./categories.types";
+
 // Gets the categories portion of the redux store state
-const selectCategoriesReducer = state => state.categories;
+const selectCategoriesReducer = (state): CategoriesState => state.categories;
 
 // Selector that looks at the categories state and gets the categories from that state
 export const selectCategories = createSelector(
@@ -15,12 +18,12 @@ export const selectCategories = createSelector(
 // Selector that looks at the categories selector and creates an array of the categories
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  categories =>
+  (categories): CategoryMap =>
     categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 // Selector that looks at the categories state and pulls the value for the isLoading item
