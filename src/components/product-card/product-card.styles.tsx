4eb32c31d13b82aa8bf7048d6e174f4ccf9f8280
Casey Macaluso <macaluso.casey@gmail.com>
2022-10-19 19:06:30 -0500

    global styling, added media queries for mobile view

--- src/components/product-card/product-card.styles.tsx
+++ src/components/product-card/product-card.styles.tsx
@@ -42,6 +42,17 @@ export const ProductCardContainer = styled.div`
       display: flex;
     }
   }
+
+  @media screen and (max-width: 800px) {
+    ${BaseButton},
+    ${GoogleButton},
+      ${InvertedButton} {
+      display: block;
+      opacity: 0.9;
+      min-width: unset;
+      padding: 0 10px;
+    }
+  }
 `;
 
 export const Footer = styled.div`
@@ -61,56 +72,3 @@ export const Footer = styled.div`
     text-align: right;
   }
 `;
-
-// .product-card-container {
-//   width: 100%;
-//   display: flex;
-//   flex-direction: column;
-//   height: 350px;
-//   align-items: center;
-//   position: relative;
-
-//   img {
-//     width: 100%;
-//     height: 95%;
-//     object-fit: cover;
-//     margin-bottom: 5px;
-//   }
-
-//   button {
-//     width: 80%;
-//     opacity: 0.7;
-//     position: absolute;
-//     top: 255px;
-//     display: none;
-//   }
-
-//   &:hover {
-//     img {
-//       opacity: 0.8;
-//     }
-
-//     button {
-//       opacity: 0.85;
-//       display: flex;
-//     }
-//   }
-
-//   .footer {
-//     width: 100%;
-//     height: 5%;
-//     display: flex;
-//     justify-content: space-between;
-//     font-size: 18px;
-
-//     .name {
-//       width: 90%;
-//       margin-bottom: 15px;
-//     }
-
-//     .price {
-//       width: 10%;
-//       text-align: right;
-//     }
-//   }
-// }
